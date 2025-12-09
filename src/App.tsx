a2e8b9076f21adb7fe2f0e96a9a341624015d707
Oliver Bryan <04oliverbryan@gmail.com>
2025-12-09 15:42:48 +0000

    biome setup

--- src/App.tsx
+++ src/App.tsx
@@ -3,31 +3,31 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [greetMsg, setGreetMsg] = useState("");
-  const [name, setName] = useState("");
+    const [greetMsg, setGreetMsg] = useState("");
+    const [name, setName] = useState("");
 
-  async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-    setGreetMsg(await invoke("greet", { name }));
-  }
+    async function greet() {
+        // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+        setGreetMsg(await invoke("greet", { name }));
+    }
 
-  return (
-    <main className="container">
-      <h1>Issue Project Manager</h1>
+    return (
+        <main className="container">
+            <h1>Issue Project Manager</h1>
 
-      <div>
-        <input
-          id="greet-input"
-          onChange={(e) => setName(e.currentTarget.value)}
-          placeholder="Enter a name..."
-        />
-        <button type="submit" onClick={greet}>
-          Greet
-        </button>
-      </div>
-      <p>{greetMsg}</p>
-    </main>
-  );
+            <div>
+                <input
+                    id="greet-input"
+                    onChange={(e) => setName(e.currentTarget.value)}
+                    placeholder="Enter a name..."
+                />
+                <button type="submit" onClick={greet}>
+                    Greet
+                </button>
+            </div>
+            <p>{greetMsg}</p>
+        </main>
+    );
 }
 
 export default App;
