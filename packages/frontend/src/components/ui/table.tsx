b0112852ffcd9f2230aaf472f04c7380d7891d74
Oliver Bryan <04oliverbryan@gmail.com>
2025-12-14 21:35:31 +0000

    much more compact table

--- packages/frontend/src/components/ui/table.tsx
+++ packages/frontend/src/components/ui/table.tsx
@@ -39,7 +39,7 @@ function TableRow({
         <tr
             data-slot="table-row"
             className={cn(
-                "data-[state=selected]:bg-muted border-b",
+                "data-[state=selected]:bg-muted h-[24px] border-b",
                 hoverEffect && "hover:bg-muted/40",
                 className,
             )}
@@ -53,7 +53,8 @@ function TableHead({ className, ...props }: React.ComponentProps<"th">) {
         <th
             data-slot="table-head"
             className={cn(
-                "text-foreground h-10 px-2 text-left align-middle font-medium whitespace-nowrap [&:has([role=checkbox])]:pr-0 [&>[role=checkbox]]:translate-y-[2px]",
+                "text-foreground px-2 h-[24px] text-left text-sm align-middle font-medium",
+                "whitespace-nowrap [&:has([role=checkbox])]:pr-0 [&>[role=checkbox]]:translate-y-[2px]",
                 className,
             )}
             {...props}
@@ -66,7 +67,7 @@ function TableCell({ className, ...props }: React.ComponentProps<"td">) {
         <td
             data-slot="table-cell"
             className={cn(
-                "p-2 align-middle whitespace-nowrap [&:has([role=checkbox])]:pr-0 [&>[role=checkbox]]:translate-y-[2px]",
+                "px-2 py-1 align-middle whitespace-nowrap [&:has([role=checkbox])]:pr-0 [&>[role=checkbox]]:translate-y-[2px]",
                 className,
             )}
             {...props}
