f971c17143ad201045e7dc558755d8ad0441fe92
Oliver Bryan <04oliverbryan@gmail.com>
2025-12-14 19:45:01 +0000

    IssueDetailPane component

---
+++ packages/frontend/src/components/issue-detail-pane.tsx
@@ -0,0 +1,18 @@
+import type { IssueRecord } from "@issue/shared";
+import { Button } from "@/components/ui/button";
+import { X } from "lucide-react";
+
+export function IssueDetailPane({ issue, close }: { issue: IssueRecord; close: () => void }) {
+    return (
+        <div className="flex flex-col items-end">
+            <Button variant={"dummy"} onClick={close} className="px-0 py-0 w-6 h-6">
+                <X />
+            </Button>
+
+            <div className="flex flex-col w-full p-2 gap-2">
+                <h1 className="text-md">{issue.title}</h1>
+                <p className="text-sm">{issue.description}</p>
+            </div>
+        </div>
+    );
+}
