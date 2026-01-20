11bf3e68f818c968820dd558053bde72b0340029
Oliver Bryan <04oliverbryan@gmail.com>
2026-01-20 16:58:19 +0000

    added tanstack query packages and provider components

--- packages/frontend/src/main.tsx
+++ packages/frontend/src/main.tsx
@@ -2,6 +2,8 @@ import "./App.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { QueryProvider } from "@/components/query-provider";
+import { SelectionProvider } from "@/components/selection-provider";
 import { RequireAuth, SessionProvider } from "@/components/session-provider";
 import { ThemeProvider } from "@/components/theme-provider";
 import { Toaster } from "@/components/ui/sonner";
@@ -15,37 +17,41 @@ import Test from "@/pages/Test";
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     <React.StrictMode>
         <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-            <SessionProvider>
-                <BrowserRouter>
-                    <Routes>
-                        {/* public routes */}
-                        <Route path="/" element={<Landing />} />
-                        <Route path="/font" element={<Font />} />
-                        <Route path="/login" element={<Login />} />
+            <QueryProvider>
+                <SessionProvider>
+                    <SelectionProvider>
+                        <BrowserRouter>
+                            <Routes>
+                                {/* public routes */}
+                                <Route path="/" element={<Landing />} />
+                                <Route path="/font" element={<Font />} />
+                                <Route path="/login" element={<Login />} />
 
-                        {/* authed routes */}
-                        <Route
-                            path="/app"
-                            element={
-                                <RequireAuth>
-                                    <App />
-                                </RequireAuth>
-                            }
-                        />
-                        <Route
-                            path="/test"
-                            element={
-                                <RequireAuth>
-                                    <Test />
-                                </RequireAuth>
-                            }
-                        />
+                                {/* authed routes */}
+                                <Route
+                                    path="/app"
+                                    element={
+                                        <RequireAuth>
+                                            <App />
+                                        </RequireAuth>
+                                    }
+                                />
+                                <Route
+                                    path="/test"
+                                    element={
+                                        <RequireAuth>
+                                            <Test />
+                                        </RequireAuth>
+                                    }
+                                />
 
-                        <Route path="*" element={<NotFound />} />
-                    </Routes>
-                </BrowserRouter>
-                <Toaster visibleToasts={1} duration={2000} />
-            </SessionProvider>
+                                <Route path="*" element={<NotFound />} />
+                            </Routes>
+                        </BrowserRouter>
+                        <Toaster visibleToasts={1} duration={2000} />
+                    </SelectionProvider>
+                </SessionProvider>
+            </QueryProvider>
         </ThemeProvider>
     </React.StrictMode>,
 );
